0d1d5e950d92c6ae863d741cc20abb030d31d000
lublud <luido992@gmail.com>
2015-07-11 20:03:46 +0400

    Retrieve package manager and ask for package installation

--- instart.pl
+++ instart.pl
@@ -14,6 +14,34 @@ use strict;
 use warnings;
 
 main();
+getPackageManager();
+
+sub getPackageManager {
+    if ($^O eq "linux") {
+        my @pm;
+
+        do{
+            if (-x qx(type -p $_ | tr -d "\n")) {
+                push (@pm, $_);
+                last;
+            }
+        } for qw/apt-get pacman/;
+        
+        if ($pm[0] eq "apt-get") {
+            push (@pm, "install");
+            push (@pm, "update");
+            push (@pm, "upgrade");
+        }
+        elsif ($pm[0] eq "pacman") {
+            push (@pm, "-S");
+            push (@pm, "-Syu");
+        }
+        
+
+        return @pm;
+    }
+} # getPackageManager
+
 
 sub readConfigFile {
     my @array = ();
@@ -49,7 +77,7 @@ sub menu {
     }
 
     print "\nChoice: ";
-    my $in = -1;
+    my $in = <STDIN>;
     while ($in < 0 || $in > $#array) {
         $in = <STDIN>;
         print "This option is not available...\nChoice: ";
@@ -63,8 +91,23 @@ sub menu {
 
 sub main {
     my @array = readConfigFile();
+    my @pm = getPackageManager();
     my $choice = menu(@array);
     print $array[$choice], "\n";
+    
+    print "About to execute \"sudo $pm[0] $pm[1] $array[$choice]\" ...\n";
+    my $rep = "no";
+    while (1) {
+        print "Do you want to continue (your password might be requested)? (yes/no) ";
+        chomp ($rep = <STDIN>);
+        if ("yes" eq $rep) {
+            qx (sudo $pm[0] $pm[1] $array[$choice]);
+            last;
+        }
+        elsif ("no" eq $rep) {
+            last;
+        }
+    }
 
 } # main
 
