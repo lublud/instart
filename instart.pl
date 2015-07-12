93a01f5fe5fbe911ce44f200101c0ecd106fcf4c
lublud <luido992@gmail.com>
2015-07-12 17:11:47 +0400

    Read config file

--- instart.pl
+++ instart.pl
@@ -48,54 +48,92 @@ sub getPackageManager {
 
 
 sub readConfigFile {
-    my @array = ();
+    my %array;
 
     open (CONFIGFILE, "<instart.config")
 		or die "Config file missing!\nExit program...\n";
 
     while (my $line = <CONFIGFILE>) {
         if ($line =~ m/[\w-]*:/) {
-            push (@array, split (/:\n/, $line));
+            my @elem = split (/:\n/, $line);
+            my $key = $elem[0];
+
+            $line = <CONFIGFILE>;
+            if ($line !~ m/config/) {
+                push (@{$array{$key}}, @elem);
+                next;
+            }
+            $line =~ m/(?<=\=)(.*?)(?=\s|\n)/;
+            if ("no" eq $1) {
+                push (@{$array{$key}}, @elem);
+                next;
+            }
+
+            #else yes
+            $line = <CONFIGFILE>;
+            if ($line =~ m/path/) {
+                $line =~ m/(?<=\=)(.*?)(?=\s|\n)/;
+                push (@elem, $1);
+            }
+            else {
+                print "Error detected in config file.\nExit program...\n";
+                exit;
+            }
+
+            $line = <CONFIGFILE>;
+            if ($line =~ m/default/) {
+                $line =~ m/(?<=\=)(.*?)(?=\s|\n)/;
+                push (@elem, $1);
+            }
+            else {
+                print "Error detected in config file.\nExit program...\n";
+                exit;
+            }
+            push (@{$array{$key}}, @elem);
         }
     }
 
     close (CONFIGFILE);
 
-    return @array;
+    return %array;
 
 } # readConfigFile
 
 sub menu {
-    my @array = @_;
+    my %array = @_;
 
     print "The following list present your packages that you can install ";
     print "along with their configuration file if there is any available\n";
 
-    for my $i (0 .. $#array) {
-		my $tmp = $i + 1;
-        print "\t$tmp - $array[$i]\n"
+
+    my $key;
+    foreach $key (sort keys %array) {
+        print "\t- $key\n";
     }
 
-    print "\nChoice (0 to quit): ";
-    my $in = <STDIN>;
-    while ($in < 0 || $in > $#array) {
-        $in = <STDIN>;
-        print "This option is not available...\nChoice: ";
-    }
-
-	if (0 == $in) {
+    print "\nChoice (quit to quit): ";
+    my $in = "";
+    chomp ($in = <STDIN>);
+	if ("quit" eq $in) {
 		exit;
 	}
 
-    return $in - 1;
+    while (! defined $array{$in}) {
+        print "This option is not available...\nChoice: ";
+        chomp ($in = <STDIN>);
+        if ("quit" eq $in) {
+            exit;
+        }
+    }
+
+    return $in;
 
 } # menu
 
 
-
 sub main {
 
-    my @array = readConfigFile();
+    my %array = readConfigFile();
     my @pm = getPackageManager();
 
     print " .__                 __                 __   \n";
@@ -107,16 +145,16 @@ sub main {
 
 	while (1) {
 		print "\n";
-		my $choice = menu(@array);
+		my $choice = menu(%array);
 
-		print "About to execute \"sudo $pm[0] $pm[1] $array[$choice]\" ...\n";
+		print "About to execute \"sudo $pm[0] $pm[1] $choice\" ...\n";
 		my $rep = "no";
 		while (1) {
 			print "Do you want to continue ";
 			print "(your password might be requested)? (yes/no) ";
 			chomp ($rep = <STDIN>);
 			if ("yes" eq $rep) {
-				qx (sudo $pm[0] $pm[1] $array[$choice]);
+				qx (sudo $pm[0] $pm[1] $choice);
 				last;
 			}
 			elsif ("no" eq $rep) {
