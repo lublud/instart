0fce811c68fc65daf013cca1b1a896b9a4730629
lublud <luido992@gmail.com>
2016-03-05 14:39:18 +0800

    Update clisi3 & vim

--- i3/clisi3.sh
+++ i3/clisi3.sh
@@ -80,8 +80,9 @@ do
         # foo
         fi
     
-    # set music currently playing (if any)
+    # set music currently playing (if any) as well as position/duration
     INFOMUSIC="`basename \"$(cmus-remote -C status | head -n 2 | tail -n 1 | grep -oP '/(.+)*')\"`"
+    INFOMUSIC+="echo - $(cmus-remote -C status | head -n 4 | tail -n 1 | grep -oP '[0-9]+') / $(cmus-remote -C status | head -n 3 | tail -n 1 | grep -oP '[0-9]+')"
 
     # set the text to display based on what mode is set
     case "$ACTION" in
